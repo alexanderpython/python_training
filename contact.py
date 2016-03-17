7d02a983016332ffefa970a804791a64fe6ee8d7
alexanderpython <alexander.giderman@rtlabs.ru>
2016-03-17 09:48:44 +0300

    Добавлен тест для создания нового контакта

---
+++ contact.py
@@ -0,0 +1,8 @@
+class Contact:
+
+    def __init__(self, firstname, lastname, address, telephone, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.telephone = telephone
+        self.email = email
