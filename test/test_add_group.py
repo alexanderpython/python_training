9b66c26183948d15e3e6321f2d98ad34ce23a467
alexanderpython <alexander.giderman@rtlabs.ru>
2016-03-24 08:58:12 +0300

    Выделен класс для работы с группами

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,11 +11,11 @@ def app():
     return fixture
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="fghf", header="fhfhffhfg", footer="fghfhfhfhf"))
-    app.session.logout()
+    app.group.session.login(username="admin", password="secret")
+    app.group.create(Group(name="fghf", header="fhfhffhfg", footer="fghfhfhfhf"))
+    app.group.session.logout()
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
-    app.session.logout()
+    app.group.session.login(username="admin", password="secret")
+    app.group.create(Group(name="", header="", footer=""))
+    app.group.session.logout()
