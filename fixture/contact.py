ed8e33238666a93763f7ec3dbb4ad97fd072fe45
alexanderpython <alexander.giderman@rtlabs.ru>
2016-04-06 23:20:24 +0300

    Оптимизированы переходы между страницами

--- fixture/contact.py
+++ fixture/contact.py
@@ -5,7 +5,8 @@ class ContactHelper:
 
     def open_new_contact_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("addressbook/") and len(wd.find_elements_by_link_text("Last name")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def create(self, contact):
         wd = self.app.wd
